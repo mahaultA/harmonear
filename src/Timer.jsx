31004f6bb06f50e8072b7dec82e859e9b4bcd4c3
mahaultA <mahault.andria@gmail.com>
2024-05-14 18:26:12 +0200

    [FIX] Le timer est correctement mis en pause lorsque l'utilisateur appuie sur pause. Cette nouvelle gestion corrige aussi le comportement du bouton Reset lorsque l'utilisateur appuie sur Reset alors que le timer défile

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,31 +1,33 @@
+import React, { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { PauseIcon, PlayIcon, RotateCcw } from "lucide-react";
-import React, { useState, useEffect } from "react";
 
 const Timer = () => {
   const [startTime, setStartTime] = useState(null);
   const [isRunning, setIsRunning] = useState(false);
   const [elapsedTime, setElapsedTime] = useState(0);
+  const [pausedTime, setPausedTime] = useState(0);
 
   useEffect(() => {
     let intervalId;
 
     if (isRunning) {
-      const intervalStartTime = Date.now(); // Stocker le timestamp initial
+      const intervalStartTime = Date.now() - (elapsedTime - pausedTime); // Calculer le temps de départ de l'intervalle
       setStartTime(intervalStartTime);
 
       intervalId = setInterval(() => {
         const now = Date.now();
-        const elapsed = now - intervalStartTime; // Calculer le temps écoulé depuis le début de l'intervalle
+        const elapsed = now - intervalStartTime + pausedTime; // Calculer le temps écoulé en tenant compte du temps de pause
         setElapsedTime(elapsed); // Mettre à jour le temps écoulé
       }, 10); // Mettre à jour toutes les 10 millisecondes
     } else {
       clearInterval(intervalId);
+      setPausedTime(elapsedTime); // Stocker le temps écoulé lorsque le timer est mis en pause
     }
 
     return () => clearInterval(intervalId);
-  }, [isRunning]);
+  }, [isRunning, elapsedTime, pausedTime]);
 
   const hours = Math.floor(elapsedTime / 3600000);
   const minutes = Math.floor((elapsedTime % 3600000) / 60000);
@@ -38,6 +40,7 @@ const Timer = () => {
 
   const reset = () => {
     setElapsedTime(0);
+    setPausedTime(0);
   };
 
   return (
