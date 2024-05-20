fc68c2056f89f14a7aa2a4ff90a37c19240a8cb7
mahaultA <mahault.andria@gmail.com>
2024-05-20 12:49:41 +0200

    Ajout d'une navbar (pas stylisee) et suppression du scroll souris sur la page Home (via fichier layout)

---
+++ app/NavBar.jsx
@@ -0,0 +1,40 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const Navbar = ({ children }) => {
+  const pathname = usePathname();
+
+  return (
+    <>
+      <nav className="flex flex-row justify-between max-w-5xl mx-auto px-4 py-3 sm:px-6">
+        <Link className={`link ${pathname === "/" ? "active" : ""}`} href="/">
+          Home
+        </Link>
+        <Link
+          className={`link ${pathname === "/practicer" ? "active" : ""}`}
+          href="/practicer"
+        >
+          Practicer
+        </Link>
+        <Link
+          className={`link ${pathname === "/challenge" ? "active" : ""}`}
+          href="/challenge"
+        >
+          Challenge
+        </Link>
+        <Link
+          className={`link ${pathname === "/about" ? "active" : ""}`}
+          href="/about"
+        >
+          About
+        </Link>
+        {/* <Link href="/about">About</Link> */}
+      </nav>
+      {children}
+    </>
+  );
+};
+
+export default Navbar;
--- app/page.jsx
+++ app/page.jsx
@@ -3,11 +3,14 @@
 import RandomNoteChallenge from "@/src/RandomNoteChallenge";
 import ScalePracticer from "@/src/ScalePracticer";
 import Timer from "@/src/Timer";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="flex flex-col items-center p-24">
       <h1 className="text-2xl font-bold my-4">Music Training App</h1>
+      <p>Presentation</p>
 
       <Timer />
     </main>
