da7e822c021be653cfb5f9826bec6285b1481f22
mahaultA <mahault.andria@gmail.com>
2024-05-20 20:37:40 +0200

    [MOD] Minor changes in about page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -5,18 +5,18 @@ export default function About() {
         My name is <span className="font-bold">Mahault</span> !
       </p>
       <p className="mx-auto md:mx-36 mx-10 mb-10 my-10 text-center">
-        I have been trying to recognize musical notes by ear using popular
+        I&apos;ve been trying to recognize musical notes by ear using popular
         methods of interval recognition and numerous ear training apps.
         <br />
         Despite my discipline to practice every day, I did not notice any
         significant progress
       </p>
       <p className="mx-auto md:mx-36 mx-10 mb-10 my-10 text-center">
-        Then I bought a course that uses the concept of{" "}
-        <span className="font-bold">tonal center</span> to practice ear training
-        ...
+        Then I bought a course that uses the concept of &quot;
+        <span className="font-bold">tonal center</span>&quot; to practice ear
+        training ...
       </p>
-      <p className="mx-auto md:mx-36 mx-10 mb-10 mt-10 text-center">
+      <p className="mx-auto md:mx-36 mx-10 mb-20 mt-10 text-center">
         And <span className="font-bold">WOW</span>!<br />I quickly observed
         progress : I was able to recognize the notes and chords played in the
         songs I was listening to 🎉 <br /> I developed these tools to practice
