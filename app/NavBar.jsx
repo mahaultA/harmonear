cf3067a5ba13db83a73b48c08684e8d05a80c6d4
mahaultA <mahault.andria@gmail.com>
2024-05-21 03:08:26 +0200

    [ADD] AJout d'icones dans NavBar

--- app/NavBar.jsx
+++ app/NavBar.jsx
@@ -1,4 +1,9 @@
 "use client";
+import { MicVocal } from "lucide-react";
+import { InfoIcon } from "lucide-react";
+import { Home } from "lucide-react";
+import { Info } from "lucide-react";
+import { Brain } from "lucide-react";
 // NavBar.jsx
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -8,38 +13,42 @@ const Navbar = ({ children }) => {
 
   return (
     <>
-      <nav className="flex flex-row justify-around max-w-5xl mx-auto px-4 py-3 sm:px-6">
+      <nav className="flex flex-row flex-wrap justify-around max-w-5xl mx-auto px-4 py-3 sm:px-6 shadow">
         <Link
           className={`link ${
             pathname === "/" ? "active font-bold" : ""
-          } hover:opacity-80`}
+          } hover:opacity-80 flex gap-2 items-center`}
           href="/"
         >
-          Home
+          <span>Home</span>
+          <Home size={18}></Home>
         </Link>
         <Link
           className={`link ${
             pathname === "/practicer" ? "active font-bold" : ""
-          } hover:opacity-80`}
+          } hover:opacity-80 flex gap-2 items-center`}
           href="/practicer"
         >
-          Practicer
+          <span>Practicer</span>
+          <Brain size={18}></Brain>
         </Link>
         <Link
           className={`link ${
             pathname === "/challenge" ? "active font-bold" : ""
-          } hover:opacity-80`}
+          } hover:opacity-80 flex gap-2 items-center`}
           href="/challenge"
         >
-          Challenge
+          <span>Challenge</span>
+          <MicVocal size={18}></MicVocal>
         </Link>
         <Link
           className={`link ${
             pathname === "/about" ? "active font-bold" : ""
-          } hover:opacity-80`}
+          } hover:opacity-80 flex gap-2 items-center`}
           href="/about"
         >
-          About
+          <span>About</span>
+          <Info size={18}></Info>
         </Link>
       </nav>
 
