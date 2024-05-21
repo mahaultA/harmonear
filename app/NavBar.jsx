845caf554f1f9789c66e8db02537815162aecda1
mahaultA <mahault.andria@gmail.com>
2024-05-21 03:29:53 +0200

    [MOD] Changements de l'épaisseur des icones de la navbar en fonction de la page active

--- app/NavBar.jsx
+++ app/NavBar.jsx
@@ -7,9 +7,11 @@ import { Brain } from "lucide-react";
 // NavBar.jsx
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react"; // Importez useState pour gérer l'état actif/inactif des liens
 
 const Navbar = ({ children }) => {
   const pathname = usePathname();
+  const [activeLink, setActiveLink] = useState(""); // Utilisez useState pour stocker le lien actif
 
   return (
     <>
@@ -19,36 +21,46 @@ const Navbar = ({ children }) => {
             pathname === "/" ? "active font-bold" : ""
           } hover:opacity-80 flex flex-col items-center`}
           href="/"
+          onClick={() => setActiveLink("/")}
         >
           <span>Home</span>
-          <Home size={18}></Home>
+          <Home size={18} strokeWidth={activeLink === "/" ? 2 : 1}></Home>
         </Link>
         <Link
           className={`link ${
             pathname === "/practicer" ? "active font-bold" : ""
           } hover:opacity-80 flex flex-col items-center`}
           href="/practicer"
+          onClick={() => setActiveLink("/practicer")}
         >
           <span>Practicer</span>
-          <Brain size={18}></Brain>
+          <Brain
+            size={18}
+            strokeWidth={activeLink === "/practicer" ? 2 : 1}
+          ></Brain>
         </Link>
         <Link
           className={`link ${
             pathname === "/challenge" ? "active font-bold" : ""
           } hover:opacity-80 flex flex-col items-center`}
           href="/challenge"
+          onClick={() => setActiveLink("/challenge")}
         >
           <span>Challenge</span>
-          <MicVocal size={18}></MicVocal>
+          <MicVocal
+            size={18}
+            strokeWidth={activeLink === "/challenge" ? 2 : 1}
+          ></MicVocal>
         </Link>
         <Link
           className={`link ${
             pathname === "/about" ? "active font-bold" : ""
           } hover:opacity-80 flex flex-col items-center`}
           href="/about"
+          onClick={() => setActiveLink("/about")}
         >
           <span>About</span>
-          <Info size={18}></Info>
+          <Info size={18} strokeWidth={activeLink === "/about" ? 2 : 1}></Info>
         </Link>
       </nav>
 
