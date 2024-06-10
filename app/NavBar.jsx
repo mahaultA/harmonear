ad49ade22d59dc4dc5e35552bdc90a2b9091ab0b
mahaultA <mahault.andria@gmail.com>
2024-06-10 21:52:12 +0200

    [MOD] Affichage du burger menu en navigation sur mobile

--- app/NavBar.jsx
+++ app/NavBar.jsx
@@ -1,73 +1,94 @@
 "use client";
-import { MicVocal } from "lucide-react";
-import { InfoIcon } from "lucide-react";
-import { Home } from "lucide-react";
-import { Info } from "lucide-react";
-import { Brain } from "lucide-react";
-// NavBar.jsx
+import { Menu, X } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useState, useEffect } from "react"; // Importez useState pour gérer l'état actif/inactif des liens
+import { useState, useEffect } from "react";
 
 const Navbar = ({ children }) => {
   const pathname = usePathname();
   const [activeLink, setActiveLink] = useState("");
+  const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
-    // Mettre à jour l'état de l'élément actif à chaque changement de chemin d'accès
     setActiveLink(pathname);
   }, [pathname]);
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <>
-      <nav className="flex flex-row flex-wrap justify-around max-w-5xl mx-auto px-4 py-3 sm:px-6 shadow">
+      <nav className="flex items-center justify-between max-w-5xl mx-auto px-4 py-3 sm:px-6 shadow">
         <Link
           className={`link ${
             activeLink === "/" ? "active font-bold" : ""
-          } hover:opacity-80 flex flex-col items-center`}
+          } hover:opacity-80 flex items-center`}
           href="/"
         >
-          <span>Accueil</span>
-          <Home size={18} strokeWidth={activeLink === "/" ? 2 : 1}></Home>
+          <span className="ml-2">Accueil</span>
         </Link>
-        <Link
-          className={`link ${
-            activeLink === "/pratique" ? "active font-bold" : ""
-          } hover:opacity-80 flex flex-col items-center`}
-          href="/pratique"
-        >
-          <span>Pratique</span>
-          <Brain
-            size={18}
-            strokeWidth={activeLink === "/pratique" ? 2 : 1}
-          ></Brain>
-        </Link>
-        <Link
-          className={`link ${
-            activeLink === "/challenge" ? "active font-bold" : ""
-          } hover:opacity-80 flex flex-col items-center`}
-          href="/challenge"
-        >
-          <span>Challenge</span>
-          <MicVocal
-            size={18}
-            strokeWidth={activeLink === "/challenge" ? 2 : 1}
-          ></MicVocal>
-        </Link>
-        <Link
-          className={`link ${
-            activeLink === "/apropos" ? "active font-bold" : ""
-          } hover:opacity-80 flex flex-col items-center`}
-          href="/apropos"
-        >
-          <span>À propos</span>
-          <Info
-            size={18}
-            strokeWidth={activeLink === "/apropos" ? 2 : 1}
-          ></Info>
-        </Link>
+        <button onClick={toggleMenu} className="sm:hidden">
+          {menuOpen ? <X size={24} /> : <Menu size={24} />}
+        </button>
+        {menuOpen && (
+          <div className="fixed left-0 shadow-4xl right-0 top-[3rem] p-5 pt-0 bg-slate-50 border-b border-b-white/20">
+            <Link
+              className={`link ${
+                activeLink === "/pratique" ? "active font-bold" : ""
+              } hover:opacity-80 flex items-center`}
+              href="/pratique"
+              onClick={() => setMenuOpen(false)}
+            >
+              <span>Pratique</span>
+            </Link>
+            <Link
+              className={`link ${
+                activeLink === "/challenge" ? "active font-bold" : ""
+              } hover:opacity-80 flex items-center`}
+              href="/challenge"
+              onClick={() => setMenuOpen(false)}
+            >
+              <span>Challenge</span>
+            </Link>
+            <Link
+              className={`link ${
+                activeLink === "/apropos" ? "active font-bold" : ""
+              } hover:opacity-80 flex items-center`}
+              href="/apropos"
+              onClick={() => setMenuOpen(false)}
+            >
+              <span>À propos</span>
+            </Link>
+          </div>
+        )}
+        <div className="hidden sm:flex space-x-4">
+          <Link
+            className={`link ${
+              activeLink === "/pratique" ? "active font-bold" : ""
+            } hover:opacity-80 flex items-center`}
+            href="/pratique"
+          >
+            <span className="ml-2">Pratique</span>
+          </Link>
+          <Link
+            className={`link ${
+              activeLink === "/challenge" ? "active font-bold" : ""
+            } hover:opacity-80 flex items-center`}
+            href="/challenge"
+          >
+            <span className="ml-2">Challenge</span>
+          </Link>
+          <Link
+            className={`link ${
+              activeLink === "/apropos" ? "active font-bold" : ""
+            } hover:opacity-80 flex items-center`}
+            href="/apropos"
+          >
+            <span className="ml-2">À propos</span>
+          </Link>
+        </div>
       </nav>
-
       {children}
     </>
   );
