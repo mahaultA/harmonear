ef9270f30ffe471f3f4442a1efa48c93df4f78a2
mahaultA <mahault.andria@gmail.com>
2024-06-12 18:39:39 +0200

    [MOD] Changement du texte de A propos

--- app/apropos/page.jsx
+++ app/apropos/page.jsx
@@ -5,23 +5,27 @@ export default function About() {
         Hello! Moi c&apos;est <span className="font-bold">Mahault</span> !
       </p>
       <p className="mx-auto md:mx-36 mx-10 mb-10 my-10 text-center">
-        J&apos;ai essayé de reconnaître les notes de musique à l&apos;oreille en
-        utilisant des méthodes populaires de reconnaissance d&apos;intervalles
-        et de nombreuses applications d&apos;tear training.
+        J&apos;ai toujours voulu avoir le talent de reconnaître les notes de
+        musique à l&apos;oreille. J&apos;ai testé plein de méthodes populaires
+        de reconnaissance d&apos;intervalles, des applications d&apos;ear
+        training, j&apos;ai demandé conseil à des musiciens qui ont une bonne
+        oreille.
         <br />
-        Malgré ma discipline à pratiquer tous les jours, je n&apos;ai pas
-        remarqué de progrès significatifs.
+        Malgré ma discipline à pratiquer tous les jours, je ne remarquais pas de
+        progrès significatifs. La déception.
       </p>
       <p className="mx-auto md:mx-36 mx-10 mb-10 my-10 text-center">
-        Puis, j&apos;ai suivi une formation qui utilise le concept de &quot;
-        <span className="font-bold">centre tonal</span>&quot; pour pratiquer
-        l&apos;ear training ...
+        Un jour, j&apos;ai suivi une formation qui m&apos;a fait changer de
+        paradigme. Cette formation d&apos;Ear Training base sa méthode sur le
+        concept de &quot;<span className="font-bold">centre tonal</span>&quot;
+        et pas <s>d&apos;intervalles</s>. Elle nécessite d&apos;avoir quelques
+        connaissances instrumentales et théoriques sur la musique.
       </p>
       <p className="mx-auto md:mx-36 mx-10 mb-10 my-10 text-center">
-        ... Et <span className="font-bold">WAOUH</span> !<br />
-        J&apos;ai rapidement observé des progrès : j&apos;ai pu reconnaître les
-        notes et les accords joués dans les chansons que j&apos;écoutais 🎉{" "}
-        <br /> J&apos;ai développé ces outils pour pratiquer cette méthode plus
+        ... Et depuis <span className="font-bold">WOW</span> !<br />
+        En 2 mois j&apos;étais capable de reconnaître les notes et les accords
+        joués dans la plupart des chansons que j&apos;écoutais 🎉 <br />
+        J&apos;ai développé ces outils pour pratiquer cette méthode plus
         confortablement, sans avoir besoin de posséder ou de savoir jouer
         d&apos;un instrument.
       </p>
